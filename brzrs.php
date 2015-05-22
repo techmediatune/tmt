7063cb1992913881e273b33bd6428c1d7db6efa0
techmediatune <tmtsupport@hotmail.com>
2015-05-23 00:20:35 +0600

    Create brzrs.php

---
+++ brzrs.php
@@ -0,0 +1,21 @@
+
+
+
+
+<html>
+
+<body>
+<object data="http://erohub.net/engine/classes/flashplayer/uppod.swf" type="application/x-shockwave-flash" width="600" height="450" align="center"><param name="bgcolor" value="#ffffff" /><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="movie" value="http://erohub.net/engine/classes/flashplayer/uppod.swf" /><param name="flashvars" value="st=http://erohub.net/engine/classes/flashplayer/skin.txt&amp;pl=http://erohub.net/engine/classes/flashplayer/playlist_video150-90.txt&amp;link=http://uppod.ru/vplwrqapq" /><embed src="http://erohub.net/engine/classes/flashplayer/uppod.swf" bgcolor="#ffffff" allowfullscreen="allowfullscreen" allowscriptaccess="always" flashvars="st=http://erohub.net/engine/classes/flashplayer/skin.txt&amp;pl=http://erohub.net/engine/classes/flashplayer/playlist_video150-90.txt&amp;link=http://uppod.ru/vplwrqapq" align="center" height="450" width="600" /></object>
+
+<br>
+<table align="center" width="100%" border="2" bordercolor="#CAD6B6" bgcolor="#B10909" bordercolorlight="#CFD9BB" bordercolordark="#CFD9BB">
+<tr >        
+<td><center>Embed Source : <input type="text" size="60" onfocus="this.select();" onclick="this.select();" style="font-size:13px; padding:2px 5px 2px 5px; border:1px solid #CFD3D8;" readonly value="<iframe SRC='http://techmediatune.com/live.php?page=newbrzrs' width='600' height='550' marginwidth=0 marginheight=0 hspace=0 vspace=0 frameborder=0 scrolling='no'></iframe>">
+</center></td>
+</tr>
+</table>
+
+<div> <?php include("includes/content.php"); ?> </div>
+
+</body>
+</html>
