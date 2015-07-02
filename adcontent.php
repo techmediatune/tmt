656935830d1f450ef0877ef5d625974864d39daa
techmediatune <tmtsupport@hotmail.com>
2015-07-03 02:29:52 +0600

    Create adcontent.php

---
+++ adcontent.php
@@ -0,0 +1,23 @@
+
+
+<div id="floatLayer1" style="height: 250px; left: 17px; position: absolute; top: 100px; width: 700px;">
+
+
+<iframe src="http://yllix.com/banner_show.php?section=General&amp;pub=813278&amp;format=300x250&amp;ga=g" frameborder="0" scrolling="no" width="300" height="250" marginwidth="0" marginheight="0"></iframe>
+<script type="text/javascript" src="//eclkspbn.com/adServe/banners?tid=3179_56697_1"></script>
+<br>
+<iframe src="http://yllix.com/banner_show.php?section=General&amp;pub=813278&amp;format=468x60&amp;ga=g" frameborder="0" scrolling="no" width="468" height="60" marginwidth="0" marginheight="0"></iframe>
+<!-- G&R_300x250 -->
+<script>
+	var gandr_conf = {
+		siteid : 3199,
+		slot : 8779,
+	};
+</script>
+<script src="http://www.gandrad.org/lib/ad.js"></script>
+<noscript><iframe frameborder="0" scrolling="no" width='300' height='250' src='http://nojs.green-red.com/src/?e=a&p=3199&l=8779'></iframe></noscript>
+<!-- End of G&R_300x250 -->
+
+<div style="position: absolute; left: -18px; top: 0px; width: 10px; height: 9px; visibility: visible;">
+<span onMouseUp="document.getElementById('floatLayer1').style.display='none'"><img src="http://i.imgur.com/AW6QOE1.png"/></span></div>
+</div>
